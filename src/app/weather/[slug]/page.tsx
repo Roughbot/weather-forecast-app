98d1d5fcf3336243226b886458141887571a1faa
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-04-11 23:06:07 +0530

    title change

--- src/app/weather/[slug]/page.tsx
+++ src/app/weather/[slug]/page.tsx
@@ -1,7 +1,12 @@
+import Head from "next/head";
+
 const Page = ({ params }: { params: any }) => {
   const { slug } = params;
   return (
     <div className="h-screen">
+      <Head>
+        <title>Weather | {slug}</title>
+      </Head>
       <p className="text-center text-black font-bold text-3xl pt-10">
         Weather Forecast: <span className="text-red-400">{slug}</span>
       </p>
