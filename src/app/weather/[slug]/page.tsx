52b46102c79a66e75a93f9102240695b037fafce
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-04-11 22:26:00 +0530

    type error

--- src/app/weather/[slug]/page.tsx
+++ src/app/weather/[slug]/page.tsx
@@ -1,9 +1,18 @@
-export default function Page({ params }: { params: { slug: string } }) {
+export default function Page({ slug }: { slug: string }) {
   return (
     <div className="h-screen">
       <p className="text-center text-black font-bold text-3xl pt-10">
-        Weather Forecast: <span className="text-red-400">{params.slug}</span>
+        Weather Forecast: <span className="text-red-400">{slug}</span>
       </p>
     </div>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const { params } = context;
+  return {
+    props: {
+      slug: params.slug,
+    },
+  };
 }
