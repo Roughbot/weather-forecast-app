789d4da2f1a8401dc7516f8c71af31b51a173aec
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-04-11 22:32:29 +0530

    fix

--- src/app/weather/[slug]/page.tsx
+++ src/app/weather/[slug]/page.tsx
@@ -1,9 +1,22 @@
-export default function Page({ params }: any) {
+const page = ({ params }: any) => {
+  const { slug } = params;
   return (
     <div className="h-screen">
       <p className="text-center text-black font-bold text-3xl pt-10">
-        Weather Forecast: <span className="text-red-400">{params.slug}</span>
+        Weather Forecast: <span className="text-red-400">{slug}</span>
       </p>
     </div>
   );
-}
+};
+
+export default page;
+
+// export default function Page({ params }: any) {
+//   return (
+//     <div className="h-screen">
+//       <p className="text-center text-black font-bold text-3xl pt-10">
+//         Weather Forecast: <span className="text-red-400">{params.slug}</span>
+//       </p>
+//     </div>
+//   );
+// }
