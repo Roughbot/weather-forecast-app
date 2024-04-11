a4105257f666c2715638c5d3a2a3d8956d3f9390
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-04-11 22:54:32 +0530

    new

--- pages/weather/[slug]/page.tsx
+++ pages/weather/[slug]/page.tsx
@@ -1,3 +1,24 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>;
+const Page = ({ slug }: { slug: string }) => {
+  return (
+    <div className="h-screen">
+      <p className="text-center text-black font-bold text-3xl pt-10">
+        Weather Forecast: <span className="text-red-400">{slug}</span>
+      </p>
+    </div>
+  );
+};
+
+export default Page;
+
+export async function getServerSideProps({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const slug = params.slug;
+  return {
+    props: {
+      slug,
+    },
+  };
 }
