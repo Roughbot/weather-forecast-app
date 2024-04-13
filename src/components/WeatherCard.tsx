652e765b8a61b1bcc904dd26e0a9e3012fe8e942
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-04-13 20:48:23 +0530

    change

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -8,16 +8,8 @@ const WeatherCard = ({ cityName }: { cityName: string }) => {
   }, [cityName]);
 
   const weatherDataFunction = async (slug: string) => {
-    const response = await fetch(
-      `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?where=%22${slug}%22`
-    );
-    const data = await response.json();
-
-    const latitude = data.results[0].coordinates.lat;
-    const longitude = data.results[0].coordinates.lon;
-
     const weatherData = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${slug}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=imperial`
     );
 
     const responseData = await weatherData.json();
