c2192e06659c2a050e4907781cdb0a80611e74cc
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-05-03 20:36:42 +0530

    temperature converter

--- src/components/fiveDaysWeather.tsx
+++ src/components/fiveDaysWeather.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { weatherDetailsProps } from "./weatherDetails";
 import Image from "next/image";
 import WeatherDetails from "@/components/weatherDetails";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTemperatureUnit } from "@/features/convertSlice";
+import { convertCelciusToFarenheit } from "@/app/utils/tempConverter";
 
 export interface forecastWeatherDetailsProps extends weatherDetailsProps {
   weatherIcon: string;
@@ -26,6 +29,21 @@ const FiveDaysWeather = (props: forecastWeatherDetailsProps) => {
     temp_max,
   } = props;
 
+  const dispatch = useDispatch();
+  const isCelsius = useSelector((state: any) => state.temperature.isCelsius);
+
+  const handleToggleTemperatureUnit = () => {
+    dispatch(toggleTemperatureUnit());
+  };
+
+  const displayTemperature = (temp: number) => {
+    if (isCelsius) {
+      return `${Math.floor(temp)}°C`;
+    } else {
+      return `${Math.floor(convertCelciusToFarenheit(temp))}°F`;
+    }
+  };
+
   return (
     <div className="w-full bg-white border rounded-lg glass_morphism flex sm:flex-row flex-col py-4 shadow-md gap-4">
       <section className="flex gap-4 justify-around items-center px-4">
@@ -44,16 +62,15 @@ const FiveDaysWeather = (props: forecastWeatherDetailsProps) => {
         </div>
         <div className="flex flex-col px-4 ">
           <span>
-            <span className="text-5xl">{temp}°</span>
-            <span className="text-2xl">C</span>
+            <span className="text-5xl">{displayTemperature(temp)}</span>
           </span>
           <p className="text-xs space-x-1 pt-1 whitespace-nowrap">
             <span>Feels Like</span>
-            <span>{feels_like}°C</span>
+            <span>{displayTemperature(feels_like)}</span>
           </p>
           <p className="text-xs space-x-2">
-            <span>{temp_min}°↓ </span>
-            <span>{temp_max}°↑ </span>
+            <span>{displayTemperature(temp_min)}↓ </span>
+            <span>{displayTemperature(temp_max)}↑ </span>
           </p>
           <p className="font-semibold">{description}</p>
         </div>
